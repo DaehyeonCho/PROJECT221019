20dccbca4eb70d8e4b492c848d07af05d2f74c1b
wheogus4629 <69081057+wheogus4629@users.noreply.github.com>
2022-11-01 22:55:29 +0900

    Create Challenge1.java

---
+++ Challenge1.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Challenge1 extends JFrame {
+	
+	public Challenge1() {
+		setTitle("Let's study Java");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400,200);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Challenge1();
+	}
+}
